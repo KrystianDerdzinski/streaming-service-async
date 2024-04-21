82e77498ad82c4a439fdcb3f3634e76fa50282a0
Krystian Derdzinski <krystian.derdzinski@gmail.com>
2024-04-21 11:07:27 +0200

    Home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestGoogleApiKeyComponent } from './test-google-api-key/test-google-api-key.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestGoogleApiKeyComponent],
+  imports: [RouterOutlet, TestGoogleApiKeyComponent, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  playlistId = 'PL197873A96AF34C19';
+}
