dce7ce181e8cbfada9630d5ee5a1a85730431d96
Krystian Derdzinski <krystian.derdzinski@gmail.com>
2024-05-18 14:38:13 +0200

    playlistid array

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,30 +4,34 @@ import { ApiService } from './../api.service';
 import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatCardModule, MatSlideToggleModule],
+  imports: [CommonModule, MatIconModule, MatCardModule, MatTabsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
-  playlistId = 'PL197873A96AF34C19';
-  playlistName = '';
+  readonly playlistId = [
+    'PL197873A96AF34C19',
+    'PLpkgjbXReLDIJ_dAREZBBz-Vizn9mfjSN',
+  ];
+  playlistName: string[] = [];
   playlistItems: SingleVideoInterface[] = [];
 
   ngOnInit(): void {
-    this.apiService.getPlaylistName(this.playlistId).subscribe((playlist) => {
-      this.playlistName = playlist.items[0].snippet.title;
+    this.playlistId.forEach((item) => {
+      this.apiService.getPlaylistName(item).subscribe((playlist) => {
+        this.playlistName.push(playlist.items[0].snippet.title);
+      });
     });
 
-    this.apiService.getPlaylistItems(this.playlistId).subscribe((items) => {
-      this.playlistItems = items.items;
-      console.log(this.playlistItems);
-      // wyswietl te dane przy pomocy *ngFor
-    });
+    // this.apiService.getPlaylistItems(this.playlistId).subscribe((items) => {
+    //   this.playlistItems = items.items;
+    //   console.log(this.playlistItems);
+    // });
   }
 
   constructor(private apiService: ApiService) {}
