02613ddc474c62238516e36978891058869bee63
Krystian Derdzinski <krystian.derdzinski@gmail.com>
2024-05-18 12:01:09 +0200

    safePipe

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SafePipe } from '../safe.pipe';
 
 @Component({
   selector: 'app-video',
   standalone: true,
-  imports: [],
+  imports: [SafePipe],
   templateUrl: './video.component.html',
-  styleUrl: './video.component.scss'
+  styleUrl: './video.component.scss',
 })
-export class VideoComponent {
+export class VideoComponent implements OnInit {
+  videoId = '';
+  videoUrl = '';
 
+  constructor(private route: ActivatedRoute) {
+    console.log('hello from constructor');
+  }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((item) => {
+      this.videoId = item['id'];
+      this.videoUrl =
+        'https://www.youtube.com/embed/' +
+        this.videoId +
+        '?si=0MfiEUX3LOHwPi3g';
+    });
+  }
 }
